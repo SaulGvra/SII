af00fb47b946117de7fe5e046b13bcbd9b6b67fe
SaulGvrs <saulgvrs@gmail.com>
2022-05-14 15:43:53 -0500

    comit8

--- modulos/plantilla-mod.php
+++ modulos/plantilla-mod.php
@@ -1,92 +1,9 @@
 <div class="principal container-fluid">
 
 <!-- Aquí su código -->
-<!-- Aquí su código -->
-<!-- Aquí su código -->
-<!-- Aquí su código -->
-<!-- Aquí su código -->
 
 </div>
 
 <!-- El code de abajo es para el index -->
 </div>
 </main>
-
-<div class="principal container-fluid">
-
-<!-- Aquí su código -->
-<!-- Aquí su código -->
-<!-- Aquí su código -->
-<!-- Aquí su código -->
-<!-- Aquí su código -->
-
-</div>
-
-<!-- El code de abajo es para el index -->
-</div>
-</main>
-
-<div class="principal container-fluid">
-
-<!-- Aquí su código -->
-<!-- Aquí su código -->
-<!-- Aquí su código -->
-<!-- Aquí su código -->
-<!-- Aquí su código -->
-
-</div>
-
-<!-- El code de abajo es para el index -->
-</div>
-</main>
-
-<div class="principal container-fluid">
-
-<!-- Aquí su código -->
-<!-- Aquí su código -->
-<!-- Aquí su código -->
-<!-- Aquí su código -->
-<!-- Aquí su código -->
-
-</div>
-
-<!-- El code de abajo es para el index -->
-</div>
-</main>
-
-<div class="principal container-fluid">
-
-<!-- Aquí su código -->
-<!-- Aquí su código -->
-<!-- Aquí su código -->
-<!-- Aquí su código -->
-<!-- Aquí su código -->
-
-</div>
-
-<!-- El code de abajo es para el index -->
-</div>
-</main>
-
-<div class="principal container-fluid">
-
-<!-- Aquí su código -->
-<!-- Aquí su código -->
-<!-- Aquí su código -->
-<!-- Aquí su código -->
-<!-- Aquí su código -->
-<!-- Aquí su código -->
-
-</div>
-
-<!-- El code de abajo es para el index -->
-</div>
-</main>
-
-
-<!-- x -->
-<!-- x-->
-<!-- Ax -->
-<!-- x -->
-<!-- Ax -->
-<!-- Ao -->
