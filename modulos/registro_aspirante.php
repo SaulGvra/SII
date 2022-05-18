bc858b73d2910dbf3eac871dca55933121a240ff
EduardoRZ1 <edzare69@gmail.com>
2022-05-18 09:50:20 -0500

    Registro aspirantes V2

--- modulos/registro_aspirante.php
+++ modulos/registro_aspirante.php
@@ -3,9 +3,10 @@
 <div class="paddingt row ">
       <div class="col-md-7 col-lg-8">
         <h4 class="mb-3">Datos del alumno</h4>
+        <h3 class="mb-3" id="titulos" >Información del alumno</h3>
         <form class="needs-validation" novalidate>
           <div class="row g-3">
-            <div class="col-sm-6">
+            <div class="col-sm-4">
               <label for="firstName" class="form-label">Nombre (s)</label>
               <input type="text" class="form-control" id="firstName" placeholder="" value="" required>
               <div class="invalid-feedback">
@@ -13,7 +14,7 @@
               </div>
             </div>
 
-            <div class="col-sm-6">
+            <div class="col-sm-4">
               <label for="lastName" class="form-label">Apellido Paterno</label>
               <input type="text" class="form-control" id="lastName" placeholder="" value="" required>
               <div class="invalid-feedback">
@@ -21,7 +22,7 @@
               </div>
             </div>
 
-            <div class="col-sm-6">
+            <div class="col-sm-4">
               <label for="lastName" class="form-label">Apellido Materno</label>
               <input type="text" class="form-control" id="lastName" placeholder="" value="" required>
               <div class="invalid-feedback">
@@ -29,46 +30,58 @@
               </div>
             </div>
 
-            <div class="col-12">
+            <h3 class="mb-3" id="titulos">Fecha de Nacimiento</h3>
+
+            <div class="col-sm-4">
+              <label for="firstName" class="form-label">Año</label>
+              <input type="text" class="form-control" id="firstName" placeholder="" value="" required>
+              <div class="invalid-feedback">
+              Introduce año de nacimiento.
+              </div>
+            </div>
+
+            <div class="col-sm-4">
+              <label for="lastName" class="form-label">Mes</label>
+              <input type="text" class="form-control" id="lastName" placeholder="" value="" required>
+              <div class="invalid-feedback">
+              Introduce mes de nacimiento.
+              </div>
+            </div>
+
+            <div class="col-sm-4">
+              <label for="lastName" class="form-label">Día</label>
+              <input type="text" class="form-control" id="lastName" placeholder="" value="" required>
+              <div class="invalid-feedback">
+              Introduce día de nacimiento.
+              </div>
+            </div>
+
+
+            <h3 class="mb-3" id="titulos">Información de contacto</h3>
+
+            <div class="col-3">
               <label for="address" class="form-label">Teléfono</label>
               <input type="text" class="form-control" id="address" placeholder="1234 Main St" required>
               <div class="invalid-feedback">
                 Introduce un número telefónico
               </div>
             </div>
-
-            <div class="col-12">
-              <label for="address2" class="form-label">Address 2 <span class="text-muted">(Optional)</span></label>
-              <input type="text" class="form-control" id="address2" placeholder="Apartment or suite">
-            </div>
-
-            <div class="col-md-5">
-              <label for="country" class="form-label">Country</label>
-              <select class="form-select" id="country" required>
-                <option value="">Choose...</option>
-                <option>United States</option>
-              </select>
+            
+            <div class="col-sm-7">
+              <label for="lastName" class="form-label">Dirección</label>
+              <input type="text" class="form-control" id="lastName" placeholder="" value="" required>
               <div class="invalid-feedback">
-                Please select a valid country.
+              Introduce tu dirección.
               </div>
             </div>
 
-            <div class="col-md-4">
-              <label for="state" class="form-label">State</label>
-              <select class="form-select" id="state" required>
-                <option value="">Choose...</option>
-                <option>California</option>
-              </select>
-              <div class="invalid-feedback">
-                Please provide a valid state.
-              </div>
-            </div>
+           
 
           </div>
 
           <hr class="my-4">
 
-          <button class="w-100 btn btn-primary btn-lg" type="submit">Continue to checkout</button>
+          <button class="w-100 btn btn-primary btn-lg" type="submit">Registrar</button>
         </form>
       </div>
     </div>
