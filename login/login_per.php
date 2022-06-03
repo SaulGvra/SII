b07de364584f84c8d65f21710b9785cebdf1f11b
SaulGvrs <saulgvrs@gmail.com>
2022-06-03 00:34:38 -0500

    Login personal

--- login/login_per.php
+++ login/login_per.php
@@ -9,6 +9,19 @@
 
   $query = "SELECT * FROM personal where personal.per_cve_personal='$usuario' and personal.per_contrasena = '$contra';";
   
+  if ($result = $mysqli->query($query)) {
+    //Da vuelta por cada fila encontrada
+    while ($row = $result->fetch_assoc()) 
+    {
+        $field1name = $row["alu_nombre"];
+        $field2name = $row["alu_paterno"];
+        $field3name = $row["alu_materno"];
+        $field4name = $row["alu_fecha"];
+        $field5name = $row["alu_telefono"];
+        $field6name = $row["alu_direccion"];
+    }
+  
+  }
   $result = $mysqli->query($query);
   $count = $result->num_rows; 
 
